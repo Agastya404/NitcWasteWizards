f4a548d9c2553950855733e2dca5940e5300c215
khushi <khushboopatidar1111@gmail.com>
2023-03-13 15:19:24 +0530

    post complaint page

---
+++ app/src/main/java/com/example/wastemanagement/UserDetails.java
@@ -0,0 +1,12 @@
+package com.example.wastemanagement;
+
+public class UserDetails {
+    public String username,email;
+
+    public UserDetails() {}
+
+    public UserDetails(String name, String email) {
+        this.username = username;
+        this.email = email;
+    }
+}
