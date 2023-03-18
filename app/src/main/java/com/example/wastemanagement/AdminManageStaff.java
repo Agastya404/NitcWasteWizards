58ccb17d41d861eeb2d3fc85b07ee6bcfd3f4978
Rekha440 <Rekha7503831347@gmail.com>
2023-03-19 00:25:07 +0530

    2 activity addes complaint succesfully and user profile

--- app/src/main/java/com/example/wastemanagement/AdminManageStaff.java
+++ app/src/main/java/com/example/wastemanagement/AdminManageStaff.java
@@ -1,18 +1,58 @@
 package com.example.wastemanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TableLayout;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AdminManageStaff extends AppCompatActivity {
 
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference reference = database.getReference();
+
+
+    Button AddStaff;
+    Button UpdateStaff;
+    Button ShowStaff;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_manage_staff);
+        AddStaff = findViewById(R.id.addstaff);
+        UpdateStaff=findViewById(R.id.update_staff);
+        ShowStaff=findViewById(R.id.showstaffdetail);
 
-        TableLayout
+        AddStaff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AdminManageStaff.this, AddStaff.class));
+
+            }
+        });
+
+        UpdateStaff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        ShowStaff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
 
     }
 }
