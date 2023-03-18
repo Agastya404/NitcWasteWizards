11e2e379c8a4a3b24e862d373fcf37631845fb01
Rekha440 <Rekha7503831347@gmail.com>
2023-03-18 13:44:06 +0530

    2 activity addes complaint succesfully and user profile

--- app/src/main/java/com/example/wastemanagement/UserDashboardPage.java
+++ app/src/main/java/com/example/wastemanagement/UserDashboardPage.java
@@ -6,12 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class UserDashboardPage extends AppCompatActivity {
 
     Button lodgeComplaint;
     Button viewComplaint;
     Button viewStatus;
+
+    Button userLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,12 +24,21 @@ public class UserDashboardPage extends AppCompatActivity {
         lodgeComplaint=findViewById(R.id.lodgeComplaint);
         viewComplaint=findViewById(R.id.viewComplaints);
         viewStatus=findViewById(R.id.viewSatuts);
+        userLogout=findViewById(R.id.userLogout);
 
         lodgeComplaint.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 startActivity(new Intent(UserDashboardPage.this, PostComplaintPage.class));
+            }
+        });
+        userLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(UserDashboardPage.this, "Logout Successful", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(UserDashboardPage.this,MainActivity.class ));
+
             }
         });
 
